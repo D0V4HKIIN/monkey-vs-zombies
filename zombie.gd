7949dac91b6ee373b2e902652d8b850e5f0e9656
hanba478 <hanba478@student.liu.se>
2024-11-30 12:00:08 +0100

    game over scene

--- zombie.gd
+++ zombie.gd
@@ -3,13 +3,16 @@ extends Node2D
 @export var SPEED = 30
 var word = "BRAIN"
 var current = 0
+var game_over_scene = preload("res://game_over.tscn")
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	$SignText.text = "[center]" + word + "[/center]"
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	position.x -= delta * SPEED
+	position.x -= delta * SPEED*3
+	if position.x <= -150:
+		game_over()
 
 func _unhandled_input(event):
 	if event is InputEventKey:
@@ -25,3 +28,9 @@ func _unhandled_input(event):
 			for i in range(current, word.length()):
 				$SignText.text += word[i]
 			$SignText.text += "[/center]"
+			
+func game_over():
+	print("Game over")
+	var go_scene = game_over_scene.instantiate()
+	get_tree().change_scene_to_file("res://game_over.tscn")
+	
